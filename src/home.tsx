c14a30cd45957337e933810021ec6674d68db0f3
任思衡 <1229150381@qq.com>
2022-03-29 09:16:47 +0800

    first commit

---
+++ src/home.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './index.css'
+import './index.scss'
+import './index.less'
+
+type Props = {}
+
+export default function Home({}: Props) {
+  return <div className="box">我是typescript</div>
+}
